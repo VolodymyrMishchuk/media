9bd9c96bcc819b4e98dfa8c25ddd209509a0d326
Volodymyr Mishchuk <vereteno.school@gmail.com>
2024-01-18 19:30:42 +0100

    Created Redux store

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import UsersList from './components/UsersList'
+
 function App() {
     return ( 
-        <h1 className="text-3xl font-bold underline">Appricosiki!</h1>
+        <h1 className="container mx-auto">
+            <UsersList />
+        </h1>
     );
 }
 
